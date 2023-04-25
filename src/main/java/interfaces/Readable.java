687be61e2bda6bd231dc57579d124e9b16809db0
AlphaThrone <haikalardzi@gmail.com>
2023-04-25 21:37:21 +0700

    changes to interfaces

--- src/main/java/interfaces/Readable.java
+++ src/main/java/interfaces/Readable.java
@@ -1,13 +1,14 @@
+package interfaces;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package interfaces;
 
 /**
  *
  * @author user
  */
-public class Readable {
+public interface Readable {
     
 }
