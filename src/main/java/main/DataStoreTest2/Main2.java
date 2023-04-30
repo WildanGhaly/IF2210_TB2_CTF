00319464037d457d4157b1727d34a003e15c019c
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-05-01 01:26:00 +0700

    feat: add secondary main - main tested for string and it works, for object it fails

---
+++ src/main/java/main/DataStoreTest2/Main2.java
@@ -0,0 +1,39 @@
+package main.DataStoreTest2;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.PropertyException;
+
+public class Main2 {
+
+    public static void main(String[] args) throws JAXBException {
+        // create some sample data to save to the XML file
+        List<String> data = new ArrayList<>();
+        data.add("apple");
+        data.add("banana");
+        data.add("cherry");
+
+        // create an instance of the XmlDataAdapter class
+        XmlDataAdapter adapter = new XmlDataAdapter();
+
+        try {
+            // save the data to an XML file
+            adapter.saveData("data.xml", data);
+            System.out.println("Data saved to data.xml");
+
+            // load the data from the XML file
+            List<?> loadedData = adapter.loadData("data.xml");
+            System.out.println("Data loaded from data.xml:");
+            for (Object item : loadedData) {
+                System.out.println(item.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
