dd5f70b7467dcba345ff335da0a693b0763820df
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-04-30 23:25:25 +0700

    feat: Update XmlDataAdapter - Update XmlDataAdapter and now is runnable for build in type

--- src/main/java/main/DataStoreTest2/XmlDataAdapter.java
+++ src/main/java/main/DataStoreTest2/XmlDataAdapter.java
@@ -1,30 +1,60 @@
 package main.DataStoreTest2;
 
-import java.beans.XMLEncoder;
-import java.beans.XMLDecoder;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlRootElement;
 
 public class XmlDataAdapter implements DataAdapter {
 
     @Override
-    public void saveData(String path, List<?> data) throws IOException {
-        XMLEncoder encoder = new XMLEncoder(new FileOutputStream(path));
-        System.out.println(data);
-        encoder.writeObject(data);
-        System.out.println(data);
-        encoder.close();
+    public void saveData(String path, List<?> data) throws IOException, JAXBException {
+        // create a file object for the specified path
+        File file = new File(path);
+
+        // create a JAXB context for the data type
+        JAXBContext context = JAXBContext.newInstance(DataWrapper.class);
+
+        // create a marshaller to convert the data to XML
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        // wrap the data in a DataWrapper object
+        DataWrapper<?> wrapper = new DataWrapper<>(new ArrayList<>(data));
+
+        // write the data to the file in XML format
+        marshaller.marshal(wrapper, file);
     }
 
+
     @Override
-    public List<?> loadData(String path) throws IOException
-    {
-        List<?> data = null;
-        XMLDecoder decoder = new XMLDecoder(new FileInputStream(path));
-        data = (List<?>)decoder.readObject();
-        decoder.close();
-        return data;
+    public List<?> loadData(String path) throws IOException {
+        // create a file object for the specified path
+        File file = new File(path);
+
+        try {
+            // create a JAXB context for the data type
+            JAXBContext context = JAXBContext.newInstance(DataWrapper.class);
+
+            // create an unmarshaller to convert the XML to data
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            // unmarshal the data from the file
+            DataWrapper<?> wrapper = (DataWrapper<?>) unmarshaller.unmarshal(file);
+
+            // return the data as a list
+            return new ArrayList<>(wrapper.getData());
+        } catch (JAXBException e) {
+            throw new IOException("Failed to load data from " + path, e);
+        }
     }
+
+
 }
