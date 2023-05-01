3725e2fecb26b79f6fd279a420408d263e37dae6
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-05-02 02:14:08 +0700

    fix: Update Main - Main.java on DataStoreTest2 is now runnable without problem - JSON and XML are now valid for data store

--- src/main/java/main/DataStoreTest2/Main.java
+++ src/main/java/main/DataStoreTest2/Main.java
@@ -2,7 +2,10 @@ package main.DataStoreTest2;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.PropertyException;
+
+import java.io.Console;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -19,6 +22,7 @@ public class Main {
         DataAdapter adapterJSON = new JsonDataAdapter();
         DataAdapter adapterXML = new XmlDataAdapter();
 
+
         try {
             List<?> data = adapterJSON.loadData(PRODUCT_FILE_JSON);
             for (int i = 0; i < 10; i++){
@@ -34,6 +38,7 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+
         try {
             List<?> data = adapterJSON.loadData(CUSTOMER_FILE_JSON);
             for (int i = 0; i < 10; i++){
@@ -45,30 +50,44 @@ public class Main {
             e.printStackTrace();
         }
 
-        try {
 
-            adapterXML.saveData(CUSTOMER_FILE_XML, List.of(customer));
+        try {
+            List<Customer> dataCustomer = new ArrayList<>();
+            dataCustomer.add(customer);
+
+            adapterXML.saveData(CUSTOMER_FILE_XML, dataCustomer);
+            System.out.println("Data saved");
+            
             List<?> data = adapterXML.loadData(CUSTOMER_FILE_XML);
-            System.out.println(data);
+            System.out.println("loaded: " + data);
+
             for (int i = 0; i < 10; i++){
-                ((List<Object>) data).add(i, customer);
+                ((List<Object>) data).add(customer);
             }
             adapterXML.saveData(CUSTOMER_FILE_XML, data);
             System.out.println(data);
         } catch (IOException | JAXBException e) {
             e.printStackTrace();
-        }
+        }        
 
-        // try {
-        //     adapterXML.saveData(PRODUCT_FILE_XML, List.of(product));
-        //     List<?> data = adapterXML.loadData(PRODUCT_FILE_XML);
-        //     for (int i = 0; i < 10; i++){
-        //         ((List<Object>) data).add(i, product);
-        //     }
-        //     adapterXML.saveData(PRODUCT_FILE_XML, data);
-        //     System.out.println(data);
-        // } catch (IOException | JAXBException e) {
-        //     e.printStackTrace();
-        // }
+
+        try {
+            List<Product> dataProduct = new ArrayList<>();
+            dataProduct.add(product);
+
+            adapterXML.saveData(PRODUCT_FILE_XML, dataProduct);
+            System.out.println("Data saved");
+            
+            List<?> data = adapterXML.loadData(PRODUCT_FILE_XML);
+            System.out.println("loaded: " + data);
+
+            for (int i = 0; i < 10; i++){
+                ((List<Object>) data).add(product);
+            }
+            adapterXML.saveData(PRODUCT_FILE_XML, data);
+            System.out.println(data);
+        } catch (IOException | JAXBException e) {
+            e.printStackTrace();
+        } 
     }
 }
