b208521feb05b8c98dc222adeab054c20f5471bc
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-05-03 17:16:49 +0700

    feat: add Member - Member here is the class that extends Customer - Member has Discount rate of 1% - Member will be extended by VIP

---
+++ src/main/java/main/DataStoreTest2/CustomerStatus/Member.java
@@ -0,0 +1,43 @@
+package main.DataStoreTest2.CustomerStatus;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Member extends Customer {
+
+    private static final double DISCOUNT_RATE_MEMBER = 0.01;
+    
+    private String name;
+    private String phoneNumber;
+
+    public Member() {
+        super();
+    }
+
+    public Member(int id, String name, String phoneNumber) {
+        super(id);
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public void addOrder(double orderAmount) {
+        super.addOrder(orderAmount);
+        int newPoints = (int) (orderAmount * DISCOUNT_RATE_MEMBER);
+        this.points += newPoints;
+    }
+
+    public double applyPoints(double amount) {
+        double discount = Math.min(amount, this.points);
+        this.points -= discount;
+        return discount;
+    }
+}
