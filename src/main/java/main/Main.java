d4598ea92cc370e758ac47f00b9efae5bd135a76
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-05-07 05:29:59 +0700

    fix: Balikin Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,15 +1,7 @@
 package main;
 
 
-import javax.swing.JFrame;
-// import java.util.jar.*;
-// import java.util.*;
-// import java.lang.reflect.*;
-// import java.io.*;
-// import java.net.URL;
-// import java.net.URLClassLoader;
-// import plugins.*;
-// import java.awt.EventQueue;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
@@ -18,7 +10,7 @@ import javax.swing.JFrame;
  *
  * @author wilda
  */
-public class Main extends JFrame{
+public class Main {
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -52,35 +44,47 @@ public class Main extends JFrame{
             }
         });
     }
-
-    // public static void main(String[] args) throws Exception {
-    //     // String jarFilePath = "src\\main\\java\\plugins\\pie.jar";
-    //     String jarFilePath = "src\\main\\java\\plugins\\pie2.jar";
-    //     List<String> classes = new ArrayList<>();
-    //     classes = Loader.getClassNamesFromJar(jarFilePath);
-    //     for (String className : classes) {
-    //         System.out.println(className);
-    //     }
-
-    //     ArrayList<Class> availableClasses = Loader.loadJarFile(jarFilePath);
-
-    //     Class<?> c = availableClasses.get(0);
-    //     System.out.println(c.getName());
-    //     Method m = c.getDeclaredMethod("start");
-        
-    //     EventQueue.invokeLater(() -> {
-    //         try {
-    //           Object obj = c.newInstance();
-    //           m.setAccessible(true);
-    //           m.invoke(obj);
-
-    //           if (obj instanceof JFrame) {
-    //               JFrame frame = (JFrame) obj;
-    //               frame.setVisible(true);
-    //           }
-    //       } catch (Exception ex) {
-    //           ex.printStackTrace();
-    //       }
-    //    });
-    // }
 }
+
+// import javax.swing.JFrame;
+// import java.util.jar.*;
+// import java.util.*;
+// import java.lang.reflect.*;
+// import java.io.*;
+// import java.net.URL;
+// import java.net.URLClassLoader;
+// import plugins.*;
+// import java.awt.EventQueue;
+
+// public class Main extends JFrame{
+//     public static void main(String[] args) throws Exception {
+//         // String jarFilePath = "src\\main\\java\\plugins\\pie.jar";
+//         String jarFilePath = "src\\main\\java\\plugins\\pie2.jar";
+//         List<String> classes = new ArrayList<>();
+//         classes = Loader.getClassNamesFromJar(jarFilePath);
+//         for (String className : classes) {
+//             System.out.println(className);
+//         }
+
+//         ArrayList<Class> availableClasses = Loader.loadJarFile(jarFilePath);
+
+//         Class<?> c = availableClasses.get(0);
+//         System.out.println(c.getName());
+//         Method m = c.getDeclaredMethod("start");
+        
+//         EventQueue.invokeLater(() -> {
+//             try {
+//               Object obj = c.newInstance();
+//               m.setAccessible(true);
+//               m.invoke(obj);
+
+//               if (obj instanceof JFrame) {
+//                   JFrame frame = (JFrame) obj;
+//                   frame.setVisible(true);
+//               }
+//           } catch (Exception ex) {
+//               ex.printStackTrace();
+//           }
+//        });
+//     }
+// }
