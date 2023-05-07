41d4de0426ff0a886f55c55e5dae0929a2d42e0c
Muhamad Salman Hakim Alfarisi <90298168+archmans@users.noreply.github.com>
2023-05-07 22:05:37 +0700

    pengaturan (#37)
    * feat : pengaturan
    
    * feat : ketinggalan

--- src/main/java/main/featureGUI/PengaturanGUI.java
+++ src/main/java/main/featureGUI/PengaturanGUI.java
@@ -4,6 +4,11 @@
  */
 package main.featureGUI;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.JPanel;
+import main.featureGUI.Util.RequestImage;
+
 /**
  *
  * @author user
@@ -14,6 +19,7 @@ public class PengaturanGUI extends javax.swing.JPanel {
      * Creates new form Setting
      */
     private static final PengaturanGUI PENGATURAN_GUI = new PengaturanGUI();
+    private final Image statusBoxPanelImg = RequestImage.requestImage("updatemember/boxStatus.png");
 
     private PengaturanGUI() {
         initComponents();
@@ -27,26 +33,116 @@ public class PengaturanGUI extends javax.swing.JPanel {
     private void initComponents() {
 
         gudangPanel = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
+        logoPegaturan = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        statusPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(statusBoxPanelImg, 0, 0, null);
+            }
+        };
+        comboBoxFile = new main.featureGUI.Util.customcombobox.Combobox();
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
-        jLabel2.setText("PENGATURAN");
+        gudangPanel.setBackground(new java.awt.Color(40, 41, 61));
+        gudangPanel.setForeground(new java.awt.Color(72, 84, 254));
+
+        logoPegaturan.setBackground(new java.awt.Color(40, 41, 61));
+        logoPegaturan.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        logoPegaturan.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/pendaftaranmember/logoPendaftaranMember.png"))); // NOI18N
+
+        jPanel1.setBackground(new java.awt.Color(40, 41, 61));
+        jPanel1.setForeground(new java.awt.Color(40, 41, 61));
+
+        jButton1.setBackground(new java.awt.Color(40, 41, 61));
+        jButton1.setForeground(new java.awt.Color(40, 41, 61));
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/pengaturan/plugin.png"))); // NOI18N
+        jButton1.setBorderPainted(false);
+        jButton1.setContentAreaFilled(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 192, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        statusPanel.setOpaque(false);
+        statusPanel.setPreferredSize(new java.awt.Dimension(151, 48));
+
+        comboBoxFile.setBackground(new java.awt.Color(40, 41, 60, 0));
+        comboBoxFile.setForeground(new java.awt.Color(217, 217, 217));
+        comboBoxFile.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "xml", "json", "obj" }));
+        comboBoxFile.setFont(new java.awt.Font("Inter", 1, 22));
+        comboBoxFile.setLabelText("File");
+        comboBoxFile.setOpaque(false);
+        comboBoxFile.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                comboBoxFileActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout statusPanelLayout = new javax.swing.GroupLayout(statusPanel);
+        statusPanel.setLayout(statusPanelLayout);
+        statusPanelLayout.setHorizontalGroup(
+            statusPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, statusPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(comboBoxFile, javax.swing.GroupLayout.DEFAULT_SIZE, 165, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        statusPanelLayout.setVerticalGroup(
+            statusPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(statusPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(comboBoxFile, javax.swing.GroupLayout.DEFAULT_SIZE, 47, Short.MAX_VALUE)
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout gudangPanelLayout = new javax.swing.GroupLayout(gudangPanel);
         gudangPanel.setLayout(gudangPanelLayout);
         gudangPanelLayout.setHorizontalGroup(
             gudangPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, gudangPanelLayout.createSequentialGroup()
-                .addContainerGap(321, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addGap(278, 278, 278))
+            .addGroup(gudangPanelLayout.createSequentialGroup()
+                .addContainerGap(190, Short.MAX_VALUE)
+                .addGroup(gudangPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, gudangPanelLayout.createSequentialGroup()
+                        .addComponent(logoPegaturan, javax.swing.GroupLayout.PREFERRED_SIZE, 534, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(180, 180, 180))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, gudangPanelLayout.createSequentialGroup()
+                        .addGroup(gudangPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, gudangPanelLayout.createSequentialGroup()
+                                .addGap(17, 17, 17)
+                                .addComponent(statusPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(353, 353, 353))))
         );
         gudangPanelLayout.setVerticalGroup(
             gudangPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, gudangPanelLayout.createSequentialGroup()
-                .addContainerGap(223, Short.MAX_VALUE)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(210, 210, 210))
+            .addGroup(gudangPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(logoPegaturan, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(statusPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(465, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -61,9 +157,22 @@ public class PengaturanGUI extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void comboBoxFileActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboBoxFileActionPerformed
+        /*TODO find membernya didatabse*/
+        comboBoxFile.getSelectedItem();
+    }//GEN-LAST:event_comboBoxFileActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private main.featureGUI.Util.customcombobox.Combobox comboBoxFile;
     private javax.swing.JPanel gudangPanel;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel logoPegaturan;
+    private javax.swing.JPanel statusPanel;
     // End of variables declaration//GEN-END:variables
 }
